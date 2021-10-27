b30f0aa0ed1f4dc74002b91e60336d84367cd435
Evan Al Mahmud Irfan ථ <66569367+sacrobrent@users.noreply.github.com>
2021-10-27 12:57:58 +0600

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,53 @@
+echo "███████╗██╗  ██╗███╗   ██╗███████╗██╗   ██╗    ";
+echo "╚══███╔╝██║  ██║████╗  ██║╚══███╔╝██║   ██║    ";
+echo "  ███╔╝ ███████║██╔██╗ ██║  ███╔╝ ██║   ██║    ";
+echo " ███╔╝  ╚════██║██║╚██╗██║ ███╔╝  ██║   ██║    ";
+echo "███████╗     ██║██║ ╚████║███████╗╚██████╔╝    ";
+echo "╚══════╝     ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝     ";
+echo "                                               ";
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/HackingTool/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/HackingTool
+
+clear
+
+mkdir HackingTool
+
+clear
+
+cd HackingTool
+
+clear
+
+git clone https://github.com/sacrobrent/HackingTool.git
+
+clear
+
+cd HackingTool
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
